c5ce666a2d08648b76d4a955fd710db583a72d8d
MaxCandace <134840785+MaxCandace@users.noreply.github.com>
2023-10-09 19:56:54 +0200

    Activity 2 (4/10/2023)
    Revised

--- Ejercicios entregados/(4-10-2023) 1.2. Conversión y Análisis de Temperaturas/script.js
+++ Ejercicios entregados/(4-10-2023) 1.2. Conversión y Análisis de Temperaturas/script.js
@@ -1,6 +1,5 @@
 // Conversión y Análisis de Temperaturas
 
-// 2.preguntar(clg (media>30)) - 4.preguntar
 
 // Temperaturas 2010 (ºC)
 
@@ -21,7 +20,7 @@ let any2020Juliol = 29.9;
 let any2020Agost = 31.5;
 
 
-// Temperaturas medias de cada año y; si superan los 30ºC utilizando un operador ternario
+// Temperaturas medias de cada año y si superan los 30ºC utilizando un operador ternario
 
 let mitjanaAny2010 = (any2010Juny + any2010Juliol + any2010Agost)/3;
 let mitjanaAny2015 = (any2015Juny + any2015Juliol + any2015Agost)/3;
@@ -31,24 +30,46 @@ let resultado2010 = mitjanaAny2010 > 30 ? "La temperatura media de 2010 superó 
 let resultado2015 = mitjanaAny2015 > 30 ? "La temperatura media de 2015 superó los 30ºC" : "La temperatura media de 2015 no superó los 30ºC";
 let resultado2020 = mitjanaAny2020 > 30 ? "La temperatura media de 2020 superó los 30ºC" : "La temperatura media de 2020 no superó los 30ºC";
 
+console.log(resultado2010);
+console.log(resultado2015);
+console.log(resultado2020);
+
 
 // Temperaturas medias convertidas a ºF y K
 
 let mitjanaAny2010Fahrenheit = mitjanaAny2010 * 9/5 + 32;
 let mitjanaAny2010Kelvin = mitjanaAny2010 + 273.15;
+
 let mitjanaAny2015Fahrenheit = mitjanaAny2015 * 9/5 + 32;
 let mitjanaAny2015Kelvin = mitjanaAny2015 + 273.15;
+
 let mitjanaAny2020Fahrenheit = mitjanaAny2020 * 9/5 + 32;
 let mitjanaAny2020Kelvin = mitjanaAny2020 + 273.15;
 
 
-// Utilizamos variables booleanas para representar si la media de cada año > 30ºC (¡¡¡¡REVISAR!!!!)
+// Utilizamos variables booleanas para representar si la media de cada año > 30ºC
 
-let superatAny2010 = mitjanaAny2010 > 30;
-let superatAny2015 = mitjanaAny2015 > 30;
-let superatAny2020 = mitjanaAny2020 > 30;
+let superatAny2010 = false
+let superatAny2015 = false
+let superatAny2020 = false
 
-console.log(superatAny2010, superatAny2015, superatAny2020);
+if (mitjanaAny2010 > 30){
+    superatAny2010 = true;
+}else {
+    superatAny2010 = false
+}
+
+if (mitjanaAny2015 > 30){
+    superatAny2015 = true;
+}else {
+    superatAny2015 = false
+}
+
+if (mitjanaAny2020 > 30){
+    superatAny2020 = true;
+}else {
+    superatAny2020 = false
+}
 
 
 // Mostraremos en pantalla las temperaturas medias de cada año en ºC, ºF y K; así como un mensaje indicando si la temperatura media es > 30ºC
