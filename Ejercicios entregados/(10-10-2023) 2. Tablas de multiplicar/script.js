f1c018b8256a4c9700b847fce939d8857194d6be
MaxCandace <134840785+MaxCandace@users.noreply.github.com>
2023-10-11 13:45:44 +0200

    Activity 2

--- Ejercicios entregados/(10-10-2023) 2. Tablas de multiplicar/script.js
+++ Ejercicios entregados/(10-10-2023) 2. Tablas de multiplicar/script.js
@@ -1,27 +1,30 @@
 //Crea un Array multidimensional que obtenga las tablas de multiplicar del 0 al 9; imprimiendolas por separado con su propio título sin los resultados impares
 
+
+
 let tablasMultiplicar = new Array(10);
 
-for(let i = 0; i < tablasMultiplicar.length; i++){
-    tablasMultiplicar[i] = new Array(11); //Cada fila va a tener 11 columnas
+for (let i = 0; i < tablasMultiplicar.length; i++) {
+    tablasMultiplicar[i] = new Array(11); // Each row will have 11 columns
 }
 
-
-for(let fila = 0; fila < tablasMultiplicar.length; fila++){
+for (let fila = 0; fila < tablasMultiplicar.length; fila++) {
     let arrayInteriro = tablasMultiplicar[fila];
-    for(let col = 0; col < arrayInteriro.length; col++){
+    for (let col = 0; col < arrayInteriro.length; col++) {
         arrayInteriro[col] = fila * col;
     }
 }
 
+for (let i = 0; i < tablasMultiplicar.length; i++) {
+    let tableResult = "Tabla de multiplicar del " + i + ": ";
+    for (let j = 0; j < tablasMultiplicar[i].length; j++) {
+        if (tablasMultiplicar[i][j] % 2 === 0) {
+            tableResult += i + " * " + j + " = " + tablasMultiplicar[i][j] + ", ";
+        }
+    }
+    console.log(tableResult);
+}
+
+
 //Realizar bulce para que las tablas salgan con su propio título como abajo. Y también incluir que los impares se descarten
-console.log("Tabla de multiplicar del 0: " + tablasMultiplicar[0]);
-console.log("Tabla de multiplicar del 1: " + tablasMultiplicar[1]);
-console.log("Tabla de multiplicar del 2: " + tablasMultiplicar[2]);
-console.log("Tabla de multiplicar del 3: " + tablasMultiplicar[3]);
-console.log("Tabla de multiplicar del 4: " + tablasMultiplicar[4]);
-console.log("Tabla de multiplicar del 5: " + tablasMultiplicar[5]);
-console.log("Tabla de multiplicar del 6: " + tablasMultiplicar[6]);
-console.log("Tabla de multiplicar del 7: " + tablasMultiplicar[7]);
-console.log("Tabla de multiplicar del 8: " + tablasMultiplicar[8]);
-console.log("Tabla de multiplicar del 9: " + tablasMultiplicar[9]);
+
